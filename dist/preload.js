b56f593e7ae805a6f4e32040db73d770e9e69630
Happysttim <gmlsdyd1023@naver.com>
2023-11-23 06:29:39 +0900

    deleted

--- dist/preload.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-var electron = require('electron');
-
-electron.contextBridge.exposeInMainWorld('ipcRenderer', {
-    on: (channel, listener) => electron.ipcRenderer.on(channel, listener),
-    send: (channel, ...args) => electron.ipcRenderer.send(channel, args),
-    invoke: (channel, ...args) => electron.ipcRenderer.invoke(channel, args)
-});
