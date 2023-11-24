270567f45625783e8055531aad527ab39e7a4d4a
Happysttim <gmlsdyd1023@naver.com>
2023-11-24 23:10:04 +0900

    fixed

--- render/login/App.tsx
+++ render/login/App.tsx
@@ -1,14 +1,9 @@
 import React, { useState, useRef } from 'react';
 import LoginBox from './components/LoginBox';
 
-const BodyStyle = {
-    opacity: 0.8
-};
-
 const App = () => {
     return (
         <div>
-            <p>로그인</p>
             <LoginBox></LoginBox>
         </div>  
     );
