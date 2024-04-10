ba541d0e72f10e6a83fbcd34094a3b79154cb256
Happysttim <gmlsdyd1023@naver.com>
2024-04-10 11:03:41 +0900

    Fix

--- render/main/utils/date-calculator.tsx
+++ render/main/utils/date-calculator.tsx
@@ -9,23 +9,23 @@ const DateCalculator = {
     },
 
     diffWeek: (source: Date, target: Date): number => {
-        return Math.abs((source.getTime() - target.getTime()) / (1000 * 60 * 60 * 24 * 7));
+        return Math.floor(Math.abs((source.getTime() - target.getTime()) / (1000 * 60 * 60 * 24 * 7)));
     },
 
     diffDay: (source: Date, target: Date): number => {
-        return Math.abs((source.getTime() - target.getTime()) / (1000 * 60 * 60 * 24));
+        return Math.floor(Math.abs((source.getTime() - target.getTime()) / (1000 * 60 * 60 * 24)));
     },
 
     diffHours: (source: Date, target: Date): number => {
-        return Math.abs((source.getTime() - target.getTime()) / (1000 * 60 * 60));
+        return Math.floor(Math.abs((source.getTime() - target.getTime()) / (1000 * 60 * 60)));
     },
 
     diffMinutes: (source: Date, target: Date): number => {
-        return Math.abs((source.getTime() - target.getTime()) / (1000 * 60));
+        return Math.floor(Math.abs((source.getTime() - target.getTime()) / (1000 * 60)));
     },
 
     diffSeconds: (source: Date, target: Date): number => {
-        return Math.abs((source.getTime() - target.getTime()) / 1000);
+        return Math.floor(Math.abs((source.getTime() - target.getTime()) / 1000));
     },
 
 };
